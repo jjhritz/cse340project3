c64e163d24d68d9b857abaa1558762736e80cadc
John Hritz <john-j-hritz@sbcglobal.net>
2016-10-01 17:00:14 -0700

    Added freopen() for CLion stdin redirction

--- project3.cpp
+++ project3.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 int main (int argc, char* argv[])
 {
+    freopen("/home/student/ClionProjects/cse340project3/tests/test01.txt", "r", stdin);
+
     int task;
 
     if (argc < 2)
